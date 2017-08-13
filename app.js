4177a12daf8d82fb3c3da71dcf2f7fe3462bef0c
Kane Marcus <kaneofthrones@gmail.com>
2017-08-13 22:38:10 +0100

    added comments

--- app.js
+++ app.js
@@ -16,100 +16,62 @@
 //     };
 
 
-
-
-
-// //----------------------------
-
-// //function to get user input 
-
-// //
-
-// //---------------------------
-
-// //function to leeop through all keys
-
-// //---------------------------
-
-// //function to play sound
-
-// //function to stop sound
-
-// //---------------------------
-
-// //make function to change light of keys
-
-// //make function to iterate the light through the keys
-
-// //-------------------------------
-
-// //action listeners for for user input
-
-// //-------------------------------
-
-// //function to display a score
-
-// //function to get random value for the sequence pattern 
-
-
-//-------------------------test code ------------------------------------------
-
+//-------------------------------------------------------
 
 var settings = {
-    sequence: [],
+    sequence: [], //array for the sequence
     round: 0,
     playNumber: 0,
     speed: 800,
     clicked: 0
-
 }
 
 
 $(document).ready(function() {
-    var audio = $("#sound");
-
+  var audio = $("#sound");
     function animate(divid) {
-
-
-        // Increase round speed.
-        if (settings.round > 1) {
-            settings.speed = 500
+        // Increase round speed
+      if (settings.round > 1) {
+          settings.speed = 500
         }
 
-//Logic to make keys light up in sequence
+//Logic to make keys light up in sequence and to play sounds
 
-        if (divid == "a") {
-            $("#a").css("border-color", "#a50be2");
-            $("#tune").attr("src", "assets/f-sharp.wav");
-            setTimeout(function() {
-                $("#a").css("border-color", "#1d8ca3");
-            }, 200);
-        } else if (divid == "b") {
-            $("#b").css("border-color", "#a50be2");
-            $("#tune").attr("src", "assets/f-sharp.wav");
-            setTimeout(function() {
-                $("#b").css("border-color", "#1d8ca3");
-            }, 200);
-        } else if (divid == "c") {
-            $("#c").css("border-color", "#a50be2");
-            $("#tune").attr("src", "assets/f-sharp.wav");
-            setTimeout(function() {
-                $("#c").css("border-color", "#1d8ca3");
-            }, 200);
-        } else if (divid == "d") {
-            $("#d").css("border-color", "#a50be2");
-            $("#tune").attr("src", "assets/f-sharp.wav");
-            setTimeout(function() {
-                $("#d").css("border-color", "#1d8ca3");
-            }, 200);
-        }
+      if (divid == "a") {
+        $("#a").css("border-color", "#a50be2");
+        $("#tune").attr("src", "assets/f-sharp.wav");
+        setTimeout(function() {
+          $("#a").css("border-color", "#1d8ca3");
+        }, 200);
+    	} else if (divid == "b") {
+        $("#b").css("border-color", "#a50be2");
+        $("#tune").attr("src", "assets/f-sharp.wav");
+        setTimeout(function() {
+          $("#b").css("border-color", "#1d8ca3");
+        }, 200);
+    	} else if (divid == "c") {
+        $("#c").css("border-color", "#a50be2");
+        $("#tune").attr("src", "assets/f-sharp.wav");
+        setTimeout(function() {
+          $("#c").css("border-color", "#1d8ca3");
+        }, 200);
+    	} else if (divid == "d") {
+        $("#d").css("border-color", "#a50be2");
+        $("#tune").attr("src", "assets/f-sharp.wav");
+        setTimeout(function() {
+          $("#d").css("border-color", "#1d8ca3");
+        }, 200);
+    	}
+
 //play, pause and load audio
+
         audio[0].pause();
         audio[0].load();
         audio[0].play();
-
     }
+
 //function to randomise sequence 
+
     function makeId() {
         var text = "";
         var possible = "abcd";
@@ -120,7 +82,8 @@ $(document).ready(function() {
 
         }
 
-//function to display a random pattern 
+//function to display a random pattern and to loop through arrays
+
         function myLoop() {
             setTimeout(function() {
                 animate(settings.sequence[settings.playNumber]);
@@ -133,32 +96,24 @@ $(document).ready(function() {
                 }
             }, settings.speed)
         }
-
         myLoop();
-
     }
 
-
-    // LISTEN 
+    //function to get check user input 
 
     function listen() {
-
-        $("#a, #b, #c, #d").on("mousedown", function() {
-
-
-            if (this.id == settings.sequence[settings.clicked]) {
-
-                if (settings.clicked === settings.sequence.length - 1) {
-                    $("#a, #b, #c, #d").off("mousedown");
-                    settings.clicked = 0;
-                    $("#start").trigger("click");
-                } else {
-                    console.log("Right!");
-                    settings.clicked++;
+      $("#a, #b, #c, #d").on("mousedown", function() {
+        if (this.id == settings.sequence[settings.clicked]) {
+          if (settings.clicked === settings.sequence.length - 1) {
+            $("#a, #b, #c, #d").off("mousedown");
+              settings.clicked = 0;
+              $("#start").trigger("click");
+              } else {
+                console.log("Right!");
+                settings.clicked++;
                 }
-
-            } else {
-                console.log("WRONG");
+            	} else {
+                console.log("Wrong");
                 $("#fail").show();
                 $("#fail").addClass("bigEntrance");
                 audio[0].pause();
@@ -168,28 +123,22 @@ $(document).ready(function() {
                 $("#keyboard, #count").css("-webkit-filter");
                 settings.clicked = 0;
                 $("#a, #b, #c, #d").off("mousedown");
-
             }
-
         });
-
     }
 
-    //BEGIN GAME
+    //function to start game
 
     $("#a, #b, #c, #d").on("click", function() {
         animate(this.id)
     });
     $("#start").on("click", function() {
         $("#start").hide();
-        $("#keyboard, #count").css("filter");
-        $("#keyboard, #count").css("-webkit-filter");
         settings.round++;
         makeId(); // make id and play it
-        $("#count").html(settings.round);
+        $("#count").html(settings.round); //display score
         //playit();
     });
-
     $("#fail").on("click", function() {
         $("#fail").hide();
         settings.sequence = [];
@@ -199,5 +148,4 @@ $(document).ready(function() {
         settings.clicked = 0;
         $("#start").trigger("click");
     });
-
 }); 
