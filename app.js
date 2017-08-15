61574946b497c7037ebcd87d368625e4b4209a4f
Kane Marcus <kaneofthrones@gmail.com>
2017-08-15 10:23:29 +0100

    fixed alignment of score and start button

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ var settings = {
     round: 0,
     playNumber: 0,
     speed: 1000,
-    clicked: 0
+    clicked: 0,
+    highScore: 0
 }
 
 $(document).ready(function() {
@@ -68,15 +69,13 @@ $(document).ready(function() {
         }, 200);
     	}
 
-//play, pause and load audio
-
+		//pause and load audio
       audio[0].pause();
       audio[0].load();
       //audio[0].play();
     }
 
 //function to randomise sequence 
-
   function makeId() {
     var text = "";
     var possible = "abcde";
@@ -86,7 +85,6 @@ $(document).ready(function() {
         }
 
 //function to display a random pattern and to loop through arrays
-
   function myLoop() {
     setTimeout(function() {
       animate(settings.sequence[settings.playNumber]);
@@ -103,7 +101,6 @@ $(document).ready(function() {
   }
 
     //function to get check user input 
-
   function listen() {
   	$("#a, #b, #c, #d, #e").on("mousedown", function() {
       if (this.id == settings.sequence[settings.clicked]) {
@@ -125,41 +122,11 @@ $(document).ready(function() {
             $("#keyboard, #count").css("-webkit-filter");
             settings.clicked = 0;
             $("#a, #b, #c, #d, #e").off("mousedown");
-        	}
-              
+        	}          
     });
-	
-
-    //keyboard logic	
-     // $("#a, #b, #c, #d, #e").on("keydown", function(){
-     // 	var key_code = event.keyCode;
-     // 		if(this.id == settings.sequence[settings.clicked]) {
-     // 			if(settings.clicked === settings.sequence.length - 1) {
-     // 				$("#a, #b, #c, #d, #e").off("keydown");
-     // 				settings.clicked = 0;
-     // 				$("#start").trigger("click");
-     // 			} else {
-     // 				console.log("Correct");
-     // 				settings.clicked++;
-     // 			}
-     // 			}
-     // 			else {
-     // 				console.log("Wrong");
-     //          $("#fail").show();
-     //          $("#fail").addClass("bigEntrance");
-     //          audio[0].pause();
-     //          audio[0].load();
-     //          audio[0].play();
-     //          $("#keyboard, #count").css("filter");
-     //          $("#keyboard, #count").css("-webkit-filter");
-     //          settings.clicked = 0;
-     //          $("#a, #b, #c, #d, #e").off("keydown");
-     // 			} 			
-     // })
-      }
+  }
 
     //function to start game
-
     $("#a, #b, #c, #d, #e").on("click", function() {
         animate(this.id);
         audio[0].play();
@@ -173,10 +140,10 @@ $(document).ready(function() {
       $("#tune").attr("src", "assets/scale-c6.wav");
       audio[0].load();
       audio[0].play();
-        console.log('a was pressed');
-        setTimeout(function() {
-         $("#a").css("border-color", "#1d8ca3");
-         }, 200);
+	    console.log('a was pressed');
+	    setTimeout(function() {
+	    	$("#a").css("border-color", "#1d8ca3");
+	    	}, 200);
     } //S key
     else if(event.keyCode == 83) {
     	$("#b").css("border-color", "#a50be2");
@@ -187,7 +154,7 @@ $(document).ready(function() {
         $("#b").css("border-color", "#1d8ca3");
         }, 200);
         console.log('s was pressed');
-    }
+    } //D key
     else if(event.keyCode == 68) {
     	$("#c").css("border-color", "#a50be2");
       $("#tune").attr("src", "assets/scale-f6.wav");
@@ -197,7 +164,7 @@ $(document).ready(function() {
         $("#c").css("border-color", "#1d8ca3");
         }, 200);
         console.log('d was pressed');
-    }
+    } //F key
     else if(event.keyCode == 70) {
     	$("#d").css("border-color", "#a50be2");
       $("#tune").attr("src", "assets/scale-g6.wav");
@@ -207,7 +174,7 @@ $(document).ready(function() {
         $("#d").css("border-color", "#1d8ca3");
         }, 200);
         console.log('f was pressed');
-    }
+    } //G key
     else if(event.keyCode == 71) {
     	$("#e").css("border-color", "#a50be2");
       $("#tune").attr("src", "assets/scale-a6.wav");
@@ -223,7 +190,7 @@ $(document).ready(function() {
       $("#start").hide();
       settings.round++;
       makeId(); // make id and play it
-      $("#count").html(settings.round); //display score
+      $("#count").html('Round: ' + settings.round); //display score
     });
 
     $("#fail").on("click", function() {
